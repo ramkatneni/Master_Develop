61e5ac230a3ed3848975ec47e28b4c07ca82ade2
I Shivakumar <i.shivakumar@ericsson.com>
2019-07-30 07:10:37 +0530

    Blog Page Templating

---
+++ blog/blog_template.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ram Katneni: Personal portfolio.</title>
+	<meta name="description" content="Naresh Katneni: iPod collectible portfolio" />
+	<meta name="keywords" content="apple, iPod, music, collectible, art, applefan, stevejobs, applenews, iphone" />
+	<link rel="shortcut icon" href="favicon.ico">
+	
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="../css/detail.css" />
+	<link rel="stylesheet" type="text/css" href="../css/quotes.css" />
+    <link rel="stylesheet" type="text/css" href="../css/component.css" />
+    <link rel="stylesheet" href="css/base.css" />
+    <link rel="stylesheet" href="css/main.css" />
+	<script src="../js/modernizr-custom.js"></script>
+	<link  href="http://fonts.googleapis.com/css?family=Reenie+Beanie:regular" 
+rel="stylesheet" type="text/css">
+<script>
+            function goBack() {
+                window.history.back();
+            }
+        </script>
+        <style>
+              .gotop {
+            position: fixed;
+            bottom: 0;
+            right: 0;
+            z-index: 600;
+        }
+        
+        .fa-arrow-left,
+        .fa-arrow-up {
+            color: white;
+            font-size: 60px;
+            margin: 15px;
+        }
+        
+        .fa-arrow-left:hover,
+        .fa-arrow-up:hover {
+            color: #f73378;
+        }
+
+            .tooltip .tooltiptext {
+                visibility: hidden;
+                width: 120px;
+                background-color: #fff;
+                color: black;
+                text-align: center;
+                border-radius: 6px;
+                padding: 5px 0;
+                position: absolute;
+                z-index: 1;
+                bottom: 100%;
+                left: 50%;
+                margin-left: -92px;
+                margin-bottom: 5px;
+            }
+            
+            .tooltip:hover .tooltiptext {
+                visibility: visible;
+            }
+       
+            
+            .icon {
+                position: absolute;
+                top: 24px;
+                width: 60px;
+                height: 8px;
+                background-color: #fff;
+                position: relative;
+                -webkit-animation: anim-icon 2.8s ease infinite;
+                animation: anim-icon 2.8s ease infinite;
+            }
+            
+            .icon:before,
+            .icon:after {
+                content: "";
+                display: inline-block;
+                width: 60px;
+                height: 8px;
+                background-color: #fff;
+                position: absolute;
+                transition: 1s;
+            }
+            
+            .icon:before {
+                top: -24px;
+                -webkit-animation: anim-before 2.8s ease infinite;
+                animation: anim-before 2.8s ease infinite;
+            }
+            
+            .icon:after {
+                bottom: -24px;
+                -webkit-animation: anim-after 2.8s ease infinite;
+                animation: anim-after 2.8s ease infinite;
+            }
+            
+            @-webkit-keyframes anim-before {
+                0% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                25% {
+                    width: 42px;
+                    -webkit-transform: rotate(45deg) translate(27px, -10px);
+                    transform: rotate(45deg) translate(27px, -10px);
+                }
+                50% {
+                    width: 42px;
+                    -webkit-transform: rotate(45deg) translate(27px, -10px);
+                    transform: rotate(45deg) translate(27px, -10px);
+                }
+                75% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                100% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+            }
+            
+            @keyframes anim-before {
+                0% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                25% {
+                    width: 42px;
+                    -webkit-transform: rotate(45deg) translate(27px, -10px);
+                    transform: rotate(45deg) translate(27px, -10px);
+                }
+                50% {
+                    width: 42px;
+                    -webkit-transform: rotate(45deg) translate(27px, -10px);
+                    transform: rotate(45deg) translate(27px, -10px);
+                }
+                75% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                100% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+            }
+            
+            @-webkit-keyframes anim-after {
+                0% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                25% {
+                    width: 42px;
+                    -webkit-transform: rotate(-45deg) translate(27px, 10px);
+                    transform: rotate(-45deg) translate(27px, 10px);
+                }
+                50% {
+                    width: 42px;
+                    -webkit-transform: rotate(-45deg) translate(27px, 10px);
+                    transform: rotate(-45deg) translate(27px, 10px);
+                }
+                75% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                100% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+            }
+            
+            @keyframes anim-after {
+                0% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                25% {
+                    width: 42px;
+                    -webkit-transform: rotate(-45deg) translate(27px, 10px);
+                    transform: rotate(-45deg) translate(27px, 10px);
+                }
+                50% {
+                    width: 42px;
+                    -webkit-transform: rotate(-45deg) translate(27px, 10px);
+                    transform: rotate(-45deg) translate(27px, 10px);
+                }
+                75% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+                100% {
+                    width: 60px;
+                    -webkit-transform: rotate(0) translate(0, 0);
+                    transform: rotate(0) translate(0, 0);
+                }
+            }
+            
+            @-webkit-keyframes anim-icon {
+                0% {
+                    -webkit-transform: rotate(0);
+                    transform: rotate(0);
+                }
+                25% {
+                    -webkit-transform: rotate(180deg);
+                    transform: rotate(180deg);
+                }
+                50% {
+                    -webkit-transform: rotate(180deg);
+                    transform: rotate(180deg);
+                }
+                75% {
+                    -webkit-transform: rotate(360deg);
+                    transform: rotate(360deg);
+                }
+                100% {
+                    -webkit-transform: rotate(360deg);
+                    transform: rotate(360deg);
+                }
+            }
+            
+            @keyframes anim-icon {
+                0% {
+                    -webkit-transform: rotate(0);
+                    transform: rotate(0);
+                }
+                25% {
+                    -webkit-transform: rotate(180deg);
+                    transform: rotate(180deg);
+                }
+                50% {
+                    -webkit-transform: rotate(180deg);
+                    transform: rotate(180deg);
+                }
+                75% {
+                    -webkit-transform: rotate(360deg);
+                    transform: rotate(360deg);
+                }
+                100% {
+                    -webkit-transform: rotate(360deg);
+                    transform: rotate(360deg);
+                }
+            }
+        </style>
+</head>
+<body>
+<div class="page">
+	<div class="window">
+      <div class="titleBar">
+        <div class="trafficLights">
+        <div class="trafficLight close" ></div>
+          <div class="trafficLight min" ></div>
+          <div class="trafficLight max" ></div>
+        </div>
+        <div class="title"><?php echo $pageTitle ?></div>
+      </div>
+      <div class="contentArea" id="top">
+      <div class="wrapper tooltip" onclick="goBack()" style="margin-top: 15px;position: absolute;width: 70px;height: 70px;">
+                <i class="fa fa-arrow-left"></i>
+                <span class="tooltiptext" style="margin:0px;">Go Back</span>
+        </div>
+
+        <?php include $pageBody ?>
+
+        <span class="gotop tooltip">
+                <a  href="#top">
+                <i class="fa fa-arrow-up"></i>
+                </a>
+                <span class="tooltiptext">Back to<br/> Top</span>
+        </span>
+      </div>
+    </div>
+</div>
+<script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+
+</body>
+</html>
